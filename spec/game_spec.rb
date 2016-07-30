50b51819260f82b4dc6f1ad436dd7920fd2082b7
James McNeil <james.r.mcneil@gmail.com>
2016-07-31 00:20:02 +0100

    removes local variable

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,17 +6,19 @@ RSpec.describe Game do
   
   describe "#score" do
     it "returns 0 if no pins have been knocked down in the game" do
-      game = Game.new
       20.times { game.roll(0) }
       expect(game.score).to eq(0)
     end
   end
   
   it "returns 20 if each roll knocked down 1 pin" do
-    game = Game.new
     20.times { game.roll(1) }
     expect(game.score).to eq(20)
   end
+
+ # it "returns 29 if one frame was a spare and the rest downed 1 pin" do
+ #   
+ # end
 
   def roll_same_score(rolls, pins)
     rolls.times { game.roll(pins) }
