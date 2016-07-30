87c1a8fcf8b5f8846e6ac794836b6e193790e81f
James McNeil <james.r.mcneil@gmail.com>
2016-07-31 00:09:07 +0100

    refactors game mock and same rolls method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,9 @@
 require 'game'
 
 RSpec.describe Game do
+
+  let (:game) { Game.new }
+  
   describe "#score" do
     it "returns 0 if no pins have been knocked down in the game" do
       game = Game.new
@@ -13,5 +16,9 @@ RSpec.describe Game do
     game = Game.new
     20.times { game.roll(1) }
     expect(game.score).to eq(20)
+  end
+
+  def roll_same_score(rolls, pins)
+    rolls.times { game.roll(pins) }
   end
 end
